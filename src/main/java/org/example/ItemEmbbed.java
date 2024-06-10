7c70be6a902d24b0a0d59d55f4f959ae8a8bcb08
Maxime HAVEZ <havez.maxime@gmail.com>
2024-06-10 21:37:18 +0200

    Add doc

--- src/main/java/org/example/ItemEmbbed.java
+++ src/main/java/org/example/ItemEmbbed.java
@@ -11,7 +11,18 @@ import java.util.List;
 
 public class ItemEmbbed {
 
+    /**
+     * This method generates a list of classes based on the role provided.
+     * If the role is not found, it returns a list of all classes.
+     *
+     * @param role The role for which the classes are to be fetched.
+     *             The role can be one of the following: "tank", "healer", "melee", "distant", "magie".
+     *             If the role is not one of these, the method returns a list of all classes.
+     * @return A list of Classe objects corresponding to the role.
+     * If the role is not found, it returns a list of all classes.
+     */
     private static List<Classe> classesList(String role) {
+        // A map to store the role and corresponding classes
         Map<String, List<String>> roleClasses = new HashMap<>();
         roleClasses.put("tank", Arrays.asList("Paladin", "Guerrier", "Chevalier Noir", "Pistosabreur"));
         roleClasses.put("healer", Arrays.asList("Mage Blanc", "Erudit", "Astromancien", "Sage"));
@@ -19,12 +30,15 @@ public class ItemEmbbed {
         roleClasses.put("distant", Arrays.asList("Barde", "Machiniste", "Danseur"));
         roleClasses.put("magie", Arrays.asList("Mage noir", "Invocateur", "Mage Rouge"));
 
+        // A list to store the classes for the role
         List<Classe> classes = new ArrayList<>();
         if (roleClasses.containsKey(role)) {
+            // If the role is found, add the corresponding classes to the list
             for (String className : roleClasses.get(role)) {
                 classes.add(new Classe(className));
             }
         } else {
+            // If the role is not found, add all classes to the list
             for (List<String> classNames : roleClasses.values()) {
                 for (String className : classNames) {
                     classes.add(new Classe(className));
@@ -35,17 +49,30 @@ public class ItemEmbbed {
         return classes;
     }
 
+    /**
+     * This method generates a MessageEmbed object for a raid creation.
+     * It sets the title, color, and fields of the embed.
+     * It also adds a field for each role ("tank", "healer", "melee", "distant", "magie") with the class names and their counts.
+     * The method returns the built MessageEmbed object.
+     *
+     * @return A MessageEmbed object for a raid creation.
+     */
     public static MessageEmbed TestEmbed() {
+        // Create a new EmbedBuilder object
         EmbedBuilder embed = new EmbedBuilder();
+
+        // Set the title, color, and fields of the embed
         embed.setTitle("Création de raid", null);
         embed.setColor(Color.GREEN);
         embed.addField("Date", "XX mois année", true);
         embed.addField("heure", "HH:MM", true);
         embed.addField("Nb participants", "XX", true);
 
+        // Define the roles and their display names
         String[] roles = {"tank", "healer", "melee", "distant", "magie"};
         String[] roleNames = {"Tanks", "Soigneurs", "DPS Melée", "DPS Distant", "Mages"};
 
+        // Add a field for each role with the class names and their counts
         for (int i = 0; i < roles.length; i++) {
             StringBuilder roleDisplay = new StringBuilder();
             for (Classe classe : classesList(roles[i])) {
@@ -54,10 +81,12 @@ public class ItemEmbbed {
             embed.addField(roleNames[i], roleDisplay.toString(), true);
         }
 
+        // Add a blank field, set the author and footer of the embed
         embed.addBlankField(true);
         embed.setAuthor("Holy Dragon lair", null, null);
         embed.setFooter("text", null);
 
+        // Build and return the MessageEmbed object
         return embed.build();
     }
 
