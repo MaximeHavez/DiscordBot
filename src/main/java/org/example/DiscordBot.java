9f313fb3ba4d41f818149724d0bbcda38a6b09db
Maxime HAVEZ <havez.maxime@gmail.com>
2024-06-10 20:12:53 +0200

    add dotenv for secret

--- src/main/java/org/example/DiscordBot.java
+++ src/main/java/org/example/DiscordBot.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
@@ -9,11 +10,13 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class DiscordBot extends ListenerAdapter {
     public static void main(String[] args) throws InterruptedException {
-
-        JDA jda = JDABuilder.createDefault("MTI0NzEzNjU4MDM4OTMwNjQxMA.GxtlLy.HIYB9tY52ewiTsubvLZPzBrlpE_JAmFMZcnf98")
+        Dotenv dotenv = Dotenv.load();
+        String apiKey = dotenv.get("API_KEY");
+        String guildId = dotenv.get("GUILD_ID");
+        JDA jda = JDABuilder.createDefault(apiKey)
                 .addEventListeners(new BotListeners()).build().awaitReady();
 
-        Guild guild = jda.getGuildById("713953931000283157");
+        Guild guild = jda.getGuildById(guildId);
 
         if(guild != null){
             guild.upsertCommand("ping", "Ping pong").queue();
