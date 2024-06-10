7bcdb0d23751169a9230d434a6030747ed61e225
Maxime HAVEZ <havez.maxime@gmail.com>
2024-06-10 21:34:25 +0200

    Formatting the embed and optimizing the code

--- src/main/java/org/example/ItemEmbbed.java
+++ src/main/java/org/example/ItemEmbbed.java
@@ -6,147 +6,80 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 public class ItemEmbbed {
 
-    private static List<Classe> classesList(){
+    private static List<Classe> classesList(String role) {
+        Map<String, List<String>> roleClasses = new HashMap<>();
+        roleClasses.put("tank", Arrays.asList("Paladin", "Guerrier", "Chevalier Noir", "Pistosabreur"));
+        roleClasses.put("healer", Arrays.asList("Mage Blanc", "Erudit", "Astromancien", "Sage"));
+        roleClasses.put("melee", Arrays.asList("Moine", "Chevalier Dragon", "Ninja", "Samouraï", "Faucheur"));
+        roleClasses.put("distant", Arrays.asList("Barde", "Machiniste", "Danseur"));
+        roleClasses.put("magie", Arrays.asList("Mage noir", "Invocateur", "Mage Rouge"));
+
         List<Classe> classes = new ArrayList<>();
-
-        //Ajout des classes de FF
-        classes.add(new Classe("Paladin"));
-        classes.add(new Classe("Guerrier"));
-        classes.add(new Classe("Chevalier Noir"));
-        classes.add(new Classe("Pistosabreur"));
-
-        classes.add(new Classe("Mage Blanc"));
-        classes.add(new Classe("Erudit"));
-        classes.add(new Classe("Astromancien"));
-        classes.add(new Classe("Sage"));
-
-        classes.add(new Classe("Moine"));
-        classes.add(new Classe("Chevalier Dragon"));
-        classes.add(new Classe("Ninja"));
-        classes.add(new Classe("Samouraï"));
-        classes.add(new Classe("Faucheur"));
-
-        classes.add(new Classe("Barde"));
-        classes.add(new Classe("Machiniste"));
-        classes.add(new Classe("Danseur"));
-
-        classes.add(new Classe("Mage noir"));
-        classes.add(new Classe("Invocateur"));
-        classes.add(new Classe("Mage Rouge"));
+        if (roleClasses.containsKey(role)) {
+            for (String className : roleClasses.get(role)) {
+                classes.add(new Classe(className));
+            }
+        } else {
+            for (List<String> classNames : roleClasses.values()) {
+                for (String className : classNames) {
+                    classes.add(new Classe(className));
+                }
+            }
+        }
 
         return classes;
     }
 
-    public static MessageEmbed TestEmbed(){
+    public static MessageEmbed TestEmbed() {
         EmbedBuilder embed = new EmbedBuilder();
-
-        // Titre
-        embed.setTitle("Mon premier bot de raid", null);
-
-        // Couleur
+        embed.setTitle("Création de raid", null);
         embed.setColor(Color.GREEN);
-
-        // Calendrier
         embed.addField("Date", "XX mois année", true);
         embed.addField("heure", "HH:MM", true);
-        embed.addField("Nb participants","XX", true);
+        embed.addField("Nb participants", "XX", true);
 
-        // Ajout d'un espace
-        embed.addBlankField(false);
+        String[] roles = {"tank", "healer", "melee", "distant", "magie"};
+        String[] roleNames = {"Tanks", "Soigneurs", "DPS Melée", "DPS Distant", "Mages"};
 
-        // Classes
-        StringBuilder affichageClasseTank = new StringBuilder();
-        StringBuilder affichageClasseHealer = new StringBuilder();
-        StringBuilder affichageClasseMelee = new StringBuilder();
-        StringBuilder affichageClasseDistant = new StringBuilder();
-        StringBuilder affichageClasseMagie = new StringBuilder();
-        for(Classe classe : classesList()){
-            if (classesList().indexOf(classe.getNom()) <= 3){
-                String champs = CompteursClasses(classe.getNom()) + " " +
-                        classe.getNom() + "\n";
-
-                affichageClasseTank.append(champs);
-            } else if (3 < classesList().indexOf(classe.getNom()) && classesList().indexOf(classe.getNom()) <= 7) {
-                String champs = CompteursClasses(classe.getNom()) + " " +
-                        classe.getNom() + "\n";
-
-                affichageClasseHealer.append(champs);
-            }else if (7 < classesList().indexOf(classe.getNom()) && classesList().indexOf(classe.getNom()) <= 12) {
-                String champs = CompteursClasses(classe.getNom()) + " " +
-                        classe.getNom() + "\n";
-
-                affichageClasseMelee.append(champs);
-            }else if (12 < classesList().indexOf(classe.getNom()) && classesList().indexOf(classe.getNom()) <= 15) {
-                String champs = CompteursClasses(classe.getNom()) + " " +
-                        classe.getNom() + "\n";
-
-                affichageClasseDistant.append(champs);
-            }else if (15 < classesList().indexOf(classe.getNom()) && classesList().indexOf(classe.getNom()) <= 18) {
-                String champs = CompteursClasses(classe.getNom()) + " " +
-                        classe.getNom() + "\n";
-
-                affichageClasseMagie.append(champs);
+        for (int i = 0; i < roles.length; i++) {
+            StringBuilder roleDisplay = new StringBuilder();
+            for (Classe classe : classesList(roles[i])) {
+                roleDisplay.append("**").append(CompteursClasses(classe.getNom())).append("** ").append(classe.getNom()).append("\n");
             }
-
-
-
+            embed.addField(roleNames[i], roleDisplay.toString(), true);
         }
 
-        embed.addField(affichageClasseTank.toString(), "", true);
-        embed.addField(affichageClasseHealer.toString(), "", true);
-        embed.addField(affichageClasseMelee.toString(), "", true);
-        embed.addField(affichageClasseDistant.toString(), "", true);
-        embed.addField(affichageClasseMagie.toString(), "", true);
-
-//        embed.addField("Tanks", "XX mois année", true);
-//        embed.addField("Soigneurs", "HH:MM", true);
-//        embed.addField("Nb participants","XX", true);
-
-        // Ajout de l'auteur
-        embed.setAuthor("Maxime", null, null);
-
-        // paramétrage du footer
+        embed.addBlankField(true);
+        embed.setAuthor("Holy Dragon lair", null, null);
         embed.setFooter("text", null);
 
-        // image
-        //embed.setImage("https://st4.depositphotos.com/1112664/28107/i/450/depositphotos_281074218-stock-photo-test-answer-sheet-with-pencil.jpg");
-
         return embed.build();
-
     }
 
     // Inscription joueur
-    public void inscrireJoueur(String nomClasse){
+    public void inscrireJoueur(String nomClasse) {
 
-        Classe classe = classesList().stream()
+        classesList("").stream()
                 .filter(c -> c.getNom().equalsIgnoreCase(nomClasse))
-                .findFirst()
-                .orElse(null);
+                .findFirst().ifPresent(Classe::incrementerEffectif);
 
-        if(classe != null) {
-            classe.incrementerEffectif();
-        }
     }
 
     // Desinscrire joueur
-    public void desinscrireJoueur(String nomClasse){
-        Classe classe = classesList().stream()
+    public void desinscrireJoueur(String nomClasse) {
+        classesList("").stream()
                 .filter(c -> c.getNom().equalsIgnoreCase(nomClasse))
-                .findFirst()
-                .orElse(null);
+                .findFirst().ifPresent(Classe::decrementerEffectif);
 
-        if(classe != null) {
-            classe.decrementerEffectif();
-        }
     }
 
     public static String CompteursClasses(String nomClasse) {
-        Classe classe = classesList().stream()
+        Classe classe = classesList("").stream()
                 .filter(c -> c.getNom().equalsIgnoreCase(nomClasse))
                 .findFirst()
                 .orElse(null);
